23e14dd805d76c52f371f1fafa54e66b75e6793f
anikoh <anikoh@gmail.com>
2017-02-28 10:21:25 +1100

    calculator working

--- Aniko Hazi/wk04/1-mon/calculator/calculator.rb
+++ Aniko Hazi/wk04/1-mon/calculator/calculator.rb
@@ -1,25 +1,35 @@
 
-command = undefined
 
-while command != "q"
+
   puts "Enter the first number!"
-  num1 = gets().to_i
+  num1 = gets().chomp.to_i
 
   puts "Enter the second number!"
-  num2 = gets().to_i
+  num2 = gets().chomp.to_i
 
   puts "+ - * /, or q for quit "
-  command = gets()
+  command = gets().chomp
 
-  if command == "+"
-    puts(num1 + num2)
-  elsif command == "-"
-    puts(num1 - num2)
-  elsif command == "*"
-    puts num1 * num2
-  else
-    puts num1/ num2
+  while command != 'q'
+
+    if command == '+'
+      puts num1 + num2
+    elsif command == '-'
+      puts num1 - num2
+    elsif command == '*'
+      puts num1 * num2
+    else
+      puts num1/ num2
+    end
+
+
+    puts "Enter the first number!"
+    num1 = gets().chomp.to_i
+
+    puts "Enter the second number!"
+    num2 = gets().chomp.to_i
+
+    puts "+ - * /, or q for quit "
+    command = gets().chomp
+
   end
-
-
-end
